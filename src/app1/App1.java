0c1ad2ad9286ca406a3cefc6fe86c1acc1a858b8
adjho <adjho@MOJAH>
2024-05-09 12:52:06 +0800

    Github testing: 		Message changes

--- src/app1/App1.java
+++ src/app1/App1.java
@@ -15,7 +15,7 @@ public class App1 {
      */
     public static void main(String[] args) {
         // TODO code application logic here
-        System.out.println("Hello World");
+        System.out.println("Welcome to Github Testing");
     }
     
 }
